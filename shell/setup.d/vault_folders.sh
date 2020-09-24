8ed95664a6366d736b8b082b00afe462e7973fac
Alexander Hess <alexander@webartifex.biz>
2020-09-24 10:57:22 +0200

    Do not create symbolic link

--- shell/setup.d/vault_folders.sh
+++ shell/setup.d/vault_folders.sh
@@ -1,6 +1,5 @@
 echo -e '\n\033[36m\033[2m\033[1m\033[7mCreating mount folders for gocryptfs vaults\033[0m\n'
 mkdir -p "$HOME/vaults/dropbox-vault"
-ln -fs "$HOME/vaults/dropbox-vault" "$HOME/documents/vault"
 mkdir -p "$HOME/vaults/storage1-vault"
 mkdir -p "$HOME/vaults/storage2-vault"
 mkdir -p "$HOME/vaults/storage3-vault"
