35519221ad825456424a7c0c7fe0deb3c97dac73
Alexander Hess <alexander@webartifex.biz>
2021-07-23 09:47:05 +0200

    Fix wrong Python 3.5.x version

--- shell/setup.d/python.sh
+++ shell/setup.d/python.sh
@@ -84,7 +84,7 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 # The following should be updated regularly.
 pyenv install 2.7.18
-pyenv install 3.5.1
+pyenv install 3.5.10
 pyenv install 3.6.14
 pyenv install 3.7.11
 pyenv install 3.8.11
