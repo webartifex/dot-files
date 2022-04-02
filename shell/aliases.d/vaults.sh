c1887c738afafbc799d6fd82ca94865a8b6ff5b3
Alexander Hess <alexander@webartifex.biz>
2022-04-02 19:43:17 +0200

    Update cryptfs vaults

--- shell/aliases.d/vaults.sh
+++ shell/aliases.d/vaults.sh
@@ -4,25 +4,15 @@ alias backup-documents-to-storage2-vault='rsync -r -t -p -o -g -x -v --progress 
 alias backup-documents-to-storage3-vault='rsync -r -t -p -o -g -x -v --progress --delete -s --exclude "#recycle" --exclude "#snapshot" --exclude "downloads" --exclude "dropbox" /home/webartifex/documents/ /home/webartifex/vaults/storage3-vault/backups/documents/'
 
 # decrypt/un-decrypt vaults
-
-alias open-documents-vault='gocryptfs -q -extpass "pass admin/vaults/documents" $HOME/documents/vault/ $HOME/vaults/documents-vault'
+alias open-documents-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/documents" $HOME/documents/vault/ $HOME/vaults/documents-vault'
 alias close-documents-vault='fusermount -q -u $HOME/vaults/documents-vault'
-
-alias open-dropbox-vault='gocryptfs -q -extpass "pass admin/vaults/dropbox" $HOME/.dropbox/.sync/Dropbox/vault/ $HOME/vaults/dropbox-vault'
+alias open-dropbox-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/dropbox" $HOME/documents/dropbox/vault/ $HOME/vaults/dropbox-vault'
 alias close-dropbox-vault='fusermount -q -u $HOME/vaults/dropbox-vault'
-
-alias open-stick1-vault='gocryptfs -q -extpass "pass admin/vaults/sticks" /media/$USER/WA-STICK1/.vault/ $HOME/vaults/stick1-vault'
+alias open-stick1-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/sticks" /media/$USER/WA-STICK1/.vault/ $HOME/vaults/stick1-vault'
 alias close-stick1-vault='fusermount -q -u $HOME/vaults/stick1-vault'
-alias open-stick2-vault='gocryptfs -q -extpass "pass admin/vaults/sticks" /media/$USER/WA-STICK2/.vault/ $HOME/vaults/stick2-vault'
+alias open-stick2-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/sticks" /media/$USER/WA-STICK2/.vault/ $HOME/vaults/stick2-vault'
 alias close-stick2-vault='fusermount -q -u $HOME/vaults/stick2-vault'
-alias open-stick3-vault='gocryptfs -q -extpass "pass admin/vaults/sticks" /media/$USER/WA-STICK3/.vault/ $HOME/vaults/stick3-vault'
+alias open-stick3-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/sticks" /media/$USER/WA-STICK3/.vault/ $HOME/vaults/stick3-vault'
 alias close-stick3-vault='fusermount -q -u $HOME/vaults/stick3-vault'
-alias open-stick4-vault='gocryptfs -q -extpass "pass admin/vaults/sticks" /media/$USER/WA-STICK4/.vault/ $HOME/vaults/stick4-vault'
+alias open-stick4-vault='gocryptfs -q -extpass "pass getraenkemarkt/vaults/sticks" /media/$USER/WA-STICK4/.vault/ $HOME/vaults/stick4-vault'
 alias close-stick4-vault='fusermount -q -u $HOME/vaults/stick4-vault'
-
-alias open-storage1-vault='gocryptfs -q -extpass "pass admin/vaults/storages" /media/$USER/wa-storage1/ $HOME/vaults/storage1-vault'
-alias close-storage1-vault='fusermount -q -u $HOME/vaults/storage1-vault'
-alias open-storage2-vault='gocryptfs -q -extpass "pass admin/vaults/storages" /media/$USER/wa-storage2/ $HOME/vaults/storage2-vault'
-alias close-storage2-vault='fusermount -q -u $HOME/vaults/storage2-vault'
-alias open-storage3-vault='gocryptfs -q -extpass "pass admin/vaults/storages" /media/$USER/wa-storage3/ $HOME/vaults/storage3-vault'
-alias close-storage3-vault='fusermount -q -u $HOME/vaults/storage3-vault'
--- shell/setup.d/vault_folders.sh
+++ shell/setup.d/vault_folders.sh
@@ -1,5 +1,6 @@
 echo -e '\n\033[36m\033[2m\033[1m\033[7mCreating mount folders for gocryptfs vaults\033[0m\n'
 mkdir -p "$HOME/vaults/documents-vault"
+mkdir -p "$HOME/vaults/dropbox-vault"
 mkdir -p "$HOME/vaults/stick1-vault"
 mkdir -p "$HOME/vaults/stick2-vault"
 mkdir -p "$HOME/vaults/stick3-vault"
